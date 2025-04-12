757bb214450284117072b0096a26984720f78446
Sebastian Wiberg <sebastian.wiberg@gmail.com>
2025-04-12 16:16:08 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/4inarow/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });

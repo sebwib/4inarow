3fc24a8dc0e157d484fb0214f0539ddd3dcd81a1
Sebastian Wiberg <sebastian.wiberg@gmail.com>
2025-04-12 16:30:19 +0200

    revert to using rr

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/4inarow/",
+  build: {
+    outDir: "dist", // Ensure this is set correctly
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });

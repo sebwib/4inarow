c3d71b2c9635dfe7232256ae9b4adecb1e3200b8
Sebastian Wiberg <sebastian.wiberg@gmail.com>
2025-04-12 16:39:00 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/4inarow/",
-  build: {
-    outDir: "dist", // Ensure this is set correctly
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
